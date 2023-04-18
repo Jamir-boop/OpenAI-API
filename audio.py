b0ca24383370923003783cf5a704ee40a512b7d1
jeiser vargas <jeisereljamir@gmail.com>
2023-04-18 14:23:11 -0500

    improvements to log files

--- audio.py
+++ audio.py
@@ -1,19 +1,19 @@
 import os
 import openai
-from utils import log_completion, local_logger
+from utils import response_log, temp_log
 
 
 def transcript(audio_file):
     openai.organization = os.getenv("PUBLIC_ORG")
     openai.api_key = os.getenv("OPENAI_API_KEY")
-    audio_file = open("audio.m4a", "rb")
+    audio_file = open(audio_file, "rb")
     transcript = openai.Audio.transcribe(
         model="whisper-1",
         file=audio_file,
         language="es",
         temperature="0.2")
-    log_completion(transcript, 0)
-    local_logger(transcript)
+    response_log(transcript, remove_spaces=0)
+    temp_log(transcript)
 
 
-transcript("audio.m4a")
+# transcript("audio.mp3")
--- utils.py
+++ utils.py
@@ -1,29 +1,48 @@
 import datetime
+import subprocess
 
 
-def log_completion(response, clean_spaces=1):
-    response = ' '.join(str(response).strip().split('/n'))
+def response_log(response, remove_spaces):
+    """
+    Log the response to a file with a timestamp.
 
-    if clean_spaces == 1:
+    Args:
+    response (str): The response to log.
+    remove_spaces (bool): If True, remove spaces from the response.
+    """
+    response = ' '.join(str(response).strip().split('\n'))
+
+    if remove_spaces:
         response = response.replace(" ", "")
 
-    log_str = f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} {response}/n"
+    log_str = f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} {response}\n"
 
     with open("response.log", "a") as f:
         f.write(log_str)
 
 
-def local_logger(transcript):
-    with open(f"local_response{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}.log", "a") as f:
+def temp_log(transcript):
+    """
+    Log the transcript to a file with a timestamp in the filename.
+
+    Args:
+    transcript (str): The transcript to log.
+    """
+    log_filename = f"response{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+
+    with open(log_filename, "a") as f:
         f.write(str(transcript))
 
-# se requiere tener instalado FFMPEG en environment path
-def extract_audio(path_video, path_audio):
-    import subprocess
 
-    input_path = path_video
-    output_path = path_audio
-    subprocess.run(['ffmpeg', '-i', input_path, '-vn',
-                   '-acodec', 'copy', output_path])
+def extract_audio(path_video, path_audio):
+    """
+    Extract audio from a video file using FFmpeg.
+
+    Args:
+    path_video (str): The path to the input video file.
+    path_audio (str): The path to the output audio file.
+    """
+    subprocess.run(['ffmpeg', '-i', path_video, '-vn',
+                   '-acodec', 'copy', path_audio])
 
 # extract_audio("C:/Users/Jeiser Vargas/Videos/2023-04-04_11-43-58.mkv", "C:/Users/Jeiser Vargas/Downloads/audio.m4a")
